3ec8a5074b5441029a3f4154b6a406f73cddf21a
adam-conway <adam.n.conway@gmail.com>
2018-02-27 09:26:47 -0700

    Working through iteration 0 on initializes

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,18 @@
+require './modules/date_handler'
+
 class Employee
+  attr_reader :id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+  def initialize(id, name, role, start_date, end_date)
+    @id = id.to_i
+    @name = name
+    @role = role
+    sd = start_date.split('-')
+    @start_date = Date.new(sd[0].to_i, sd[1].to_i, sd[2].to_i)
+    ed = end_date.split('-')
+    @end_date = Date.new(ed[0].to_i, ed[1].to_i, ed[2].to_i)
+  end
 end
